f545fd098a0646a7dd6df433bcd8c602ffeb624b
Hunter Fox <hefoxed@gmail.com>
2015-09-13 17:26:50 -0700

    Require homework, user

--- spec/models/homework_assignment_spec.rb
+++ spec/models/homework_assignment_spec.rb
@@ -10,4 +10,10 @@ RSpec.describe HomeworkAssignment, type: :model do
     homework = FactoryGirl.create(:homework, :hw2)
     expect(FactoryGirl.build(:homework_assignment, user: student, homework: homework)).to be_valid
   end
+  it "is invalid without a user" do
+    expect(FactoryGirl.build(:homework_assignment, user: nil)).not_to be_valid
+  end
+  it "is invalid without a homework" do
+    expect(FactoryGirl.build(:homework_assignment, homework: nil)).not_to be_valid
+  end
 end
